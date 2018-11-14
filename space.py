1340e3f71aa9c0acefca90ff5f387cc6e1a9b0ad
brianhouse <brian.house@gmail.com>
2018-11-14 17:24:47 -0500

    showing 2d visualization

--- space.py
+++ space.py
@@ -5,23 +5,70 @@ import numpy as np
 from util import load, save
 from model import Model, Pair, RULES, STATES
 
+from ggplot import * # https://stackoverflow.com/questions/50591982/importerror-cannot-import-name-timestamp
+from sklearn.manifold import TSNE
+from sklearn.decomposition import PCA
+from sklearn.cluster import DBSCAN
+from pandas import DataFrame
+
+
 # # load models
 # path = os.path.abspath(os.path.join(os.path.dirname(__file__), "models.pkl"))
 # baseline, models = load(path)
 
-# # reformat as 16-dimensional points
-# matrices = [model.rules for model in models]
-# points = []
-# for matrix in matrices:
-# 	point = []
-# 	for state_1 in STATES:
-# 		for state_2 in STATES:
-# 			point.append(matrix[state_1][state_2])
-# 	points.append(point)
-# points = np.array(points)
 
-# save("points.pkl", points)
+# def linearize(model):
+#     point = []
+#     for state_1 in STATES:
+#         for state_2 in STATES:
+#             point.append(model.rules[state_1][state_2])
+#     return point
 
-points = load("points.pkl")
-print(points)
+# baseline = linearize(baseline)
+# points = np.array(list(map(linearize, models)))
+
+
+# save("points.pkl", (baseline, points))
+
+baseline, X = load("points.pkl")
+np.append(X, baseline)
+print(X.shape)
+
+def classify(x):
+    x = list(x)
+    return "%d" % x.index(max(x))
+    # if x[0] >= .5:
+    #     return "++"
+    # if x[3] >= .5:
+    #     return "--"
+    # else:
+    #     return "?"
+
+print(baseline)
+print(classify(baseline))
+
+
+types = list(map(classify, X))
+types[-1] = "20"
+
+categories = list(map(lambda x: int(x), list(set(types))))
+categories.sort()
+print(categories)
+
+# if dimensions were greater than 50, should use PCA to reduce first
+# results = TSNE(n_components=2).fit_transform(X)
+results = PCA(n_components=2).fit_transform(X)
+print(results.shape)
+
+
+# clusters = DBSCAN(eps=2, min_samples=2).fit(results)
+# print(clusters.labels_)
+# types = clusters.labels_
+
+
+results = {'x': results[:, 0], 'y': results[:, 1], 'type': types}
+
+chart = ggplot(DataFrame(results), aes(x='x', y='y', color='type')) + geom_point(size=50, alpha=1) + ggtitle("Generated models")
+chart.show()
+
 
